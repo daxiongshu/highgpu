c405e139e786368e02afecf5f87bd5c905fd8b59
daxiongshu <aixueer4ever@gmail.com>
2022-11-19 02:20:51 +0000

    krr

---
+++ krr.py
@@ -0,0 +1,24 @@
+import argparse
+parser = argparse.ArgumentParser(description='Traing gnn')
+parser.add_argument('--gpu','-g',dest='gpu',default=0)
+args = parser.parse_args()
+print(args)
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
+
+import cupy
+import gc
+from cuml import KernelRidge
+
+def run():
+    M,N = 20000,1000
+    S = 10000000
+    x = cupy.random.rand(M,N)
+    y = cupy.random.rand(M)
+    for _ in range(S):
+        for _ in range(S):
+            krr = KernelRidge(kernel='rbf')
+            krr.fit(x,y)
+            del krr
+            gc.collect()
--- nn.sh
+++ nn.sh
@@ -1,5 +1,5 @@
-pip install -r requirement.txt
-python run.py -g 0 &
+#pip install -r requirement.txt
+#python run.py -g 0 &
 python run.py -g 1 &
 python run.py -g 2 &
 python run.py -g 3 &
