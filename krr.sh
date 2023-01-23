b97bd510549ff034c4bea8803c7cdf5c9e419827
daxiongshu <aixueer4ever@gmail.com>
2023-01-23 19:36:11 +0000

    use absolute paths

--- krr.sh
+++ krr.sh
@@ -1,4 +1,4 @@
-python krr.py -g 0 &
-python krr.py -g 1 &
-python krr.py -g 2 &
-python krr.py -g 3 &
+python /highgpu/krr.py -g 0 &
+python /highgpu/krr.py -g 1 &
+python /highgpu/krr.py -g 2 &
+python /highgpu/krr.py -g 3 &
--- nn.sh
+++ nn.sh
@@ -1,5 +1,5 @@
 pip install -r requirement.txt
-python run.py -g 0 &
-python run.py -g 1 &
-python run.py -g 2 &
-python run.py -g 3 &
+python /highgpu/run.py -g 0 &
+python /highgpu/run.py -g 1 &
+python /highgpu/run.py -g 2 &
+python /highgpu/run.py -g 3 &
